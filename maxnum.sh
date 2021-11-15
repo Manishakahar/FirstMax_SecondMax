6a3f309e48593a7d6e2e3edb62fbbdcdc93eaaf6
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-15 22:09:47 +0530

    [Manisha]: Used Function To Perform Operation.

--- maxnum.sh
+++ maxnum.sh
@@ -1,15 +1,19 @@
 
+function randomNum()
+{
 for((i=0;i<10;i++))
 do
 	arr[i]=$((RANDOM%900 +100))
 done
-
+}
+randomNum
 echo "All numbers are= ${arr[@]}"
 
 firstMax=${arr[0]}
 secMax=${arr[0]}
 
-
+function findMax()
+{
 for((i=0;i<10;i++))
 do
 	if(( arr[i] >  firstMax ))
@@ -21,6 +25,7 @@ do
 		secMax=${arr[i]}
 	fi
 done
-
+}
+findMax
 echo "First Number = $firstMax"
 echo "Second Max Number= $secMax"
